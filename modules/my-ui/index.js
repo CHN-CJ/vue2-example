35c6a5bef04a3d03d93df5d0903fd926a6cb7a2c
CHN-CJ <2873183291@qq.com>
2023-01-06 15:39:35 +0800

    搭建环境

---
+++ modules/my-ui/index.js
@@ -0,0 +1,12 @@
+import MyButton from './Button';
+import MyInput from './Input';
+
+const MyUI = {
+
+}
+
+MyUI.install = function (Vue, options) {
+    console.log(options);
+}
+
+export default MyUI
--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import MyUI from '../modules/my-ui';
+
+/* 
+按需加载
+*/
+Vue.use(MyUI, {
+  components: [
+    'MyButton',
+    'MyInput'
+  ]
+});
+
+// Vue.use(MyUI);
 
 Vue.config.productionTip = false
 
